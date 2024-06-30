5f686f8de3faca87dae02813a5cab434605a8676
suganandan <suganandan@hotmail.com>
2024-06-30 14:53:10 +0530

    Adding Junit classes

---
+++ src/test/java/com/sugan/gradlespringboot/controller/HelloControllerAcceptanceTest.java
@@ -0,0 +1,24 @@
+package com.sugan.gradlespringboot.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@WebMvcTest(HelloController.class)
+class HelloControllerAcceptanceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetWelcome() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().is(HttpStatus.Series.SUCCESSFUL.value()))
+                .andExpect(content().string("Welcome to Gradle Spring boot"));
+    }
+}
